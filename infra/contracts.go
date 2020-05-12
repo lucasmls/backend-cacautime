9bbb5d372becd3780358403c210b4a3617086b27
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-12 08:41:07 -0300

    :sparkles: added the Log provider contract

--- infra/contracts.go
+++ infra/contracts.go
@@ -14,3 +14,22 @@ type DatabaseClient interface {
 	ExecuteQuery(context.Context, string, ...interface{}) (*sql.Rows, error)
 	Execute(context.Context, string, ...interface{}) (driver.Result, error)
 }
+
+// LogProvider ...
+type LogProvider interface {
+	Critical(context.Context, OpName, string)
+	Criticalf(context.Context, OpName, string, ...interface{})
+	CriticalMetadata(context.Context, OpName, string, Metadata)
+	Error(context.Context, OpName, string)
+	Errorf(context.Context, OpName, string, ...interface{})
+	ErrorMetadata(context.Context, OpName, string, Metadata)
+	Warning(context.Context, OpName, string)
+	Warningf(context.Context, OpName, string, ...interface{})
+	WarningMetadata(context.Context, OpName, string, Metadata)
+	Info(context.Context, OpName, string)
+	Infof(context.Context, OpName, string, ...interface{})
+	InfoMetadata(context.Context, OpName, string, Metadata)
+	Debug(context.Context, OpName, string)
+	Debugf(context.Context, OpName, string, ...interface{})
+	DebugMetadata(context.Context, OpName, string, Metadata)
+}
